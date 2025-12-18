bedd2a7ecb07aaa2f64e03ef706981326a327f30
qiuyu2003 <162788471+Qiuyu2024@users.noreply.github.com>
2025-12-18 14:51:40 +0800

    Add files via upload

---
+++ basicsr/Frequency_branch.py
@@ -0,0 +1,158 @@
+import math
+import torch
+import torch.nn as nn
+from torch.nn import init as init
+from basicsr.utils.utils import *
+from einops import rearrange
+NEG_INF = -1000000
+def window_partitions(x, window_size):
+    """
+    Args:
+        x: (B, C, H, W)
+        window_size (int): window size
+    Returns:
+        windows: (num_windows*B, C, window_size, window_size)
+    """
+    if isinstance(window_size, int):
+        window_size = [window_size, window_size]
+    B, C, H, W = x.shape
+    x = x.view(B, C, H // window_size[0], window_size[0], W // window_size[1], window_size[1])
+    windows = x.permute(0, 2, 4, 1, 3, 5).contiguous().view(-1, C, window_size[0], window_size[1])
+    return windows
+
+
+def window_reverses(windows, window_size, H, W):
+    """
+    Args:
+        windows: (num_windows*B, C, window_size, window_size)
+        window_size (int): Window size
+        H (int): Height of image
+        W (int): Width of image
+    Returns:
+        x: (B, C, H, W)
+    """
+    # B = int(windows.shape[0] / (H * W / window_size / window_size))
+    # print('B: ', B)
+    # print(H // window_size)
+    # print(W // window_size)
+    if isinstance(window_size, int):
+        window_size = [window_size, window_size]
+    C = windows.shape[1]
+    # print('C: ', C)
+    x = windows.view(-1, H // window_size[0], W // window_size[1], C, window_size[0], window_size[1])
+    x = x.permute(0, 3, 1, 4, 2, 5).contiguous().view(-1, C, H, W)
+    return x
+
+def window_partitionx(x, window_size):
+    _, _, H, W = x.shape
+    h, w = window_size * (H // window_size), window_size * (W // window_size)
+    x_main = window_partitions(x[:, :, :h, :w], window_size)
+    b_main = x_main.shape[0]
+    if h == H and w == W:
+        return x_main, [b_main]
+    if h != H and w != W:
+        x_r = window_partitions(x[:, :, :h, -window_size:], window_size)
+        b_r = x_r.shape[0] + b_main
+        x_d = window_partitions(x[:, :, -window_size:, :w], window_size)
+        b_d = x_d.shape[0] + b_r
+        x_dd = x[:, :, -window_size:, -window_size:]
+        b_dd = x_dd.shape[0] + b_d
+        # batch_list = [b_main, b_r, b_d, b_dd]
+        return torch.cat([x_main, x_r, x_d, x_dd], dim=0), [b_main, b_r, b_d, b_dd]
+    if h == H and w != W:
+        x_r = window_partitions(x[:, :, :h, -window_size:], window_size)
+        b_r = x_r.shape[0] + b_main
+        return torch.cat([x_main, x_r], dim=0), [b_main, b_r]
+    if h != H and w == W:
+        x_d = window_partitions(x[:, :, -window_size:, :w], window_size)
+        b_d = x_d.shape[0] + b_main
+        return torch.cat([x_main, x_d], dim=0), [b_main, b_d]
+
+def window_reversex(windows, window_size, H, W, batch_list):
+    h, w = window_size * (H // window_size), window_size * (W // window_size)
+    # print(windows[:batch_list[0], ...].shape)
+    x_main = window_reverses(windows[:batch_list[0], ...], window_size, h, w)
+    B, C, _, _ = x_main.shape
+    # print('windows: ', windows.shape)
+    # print('batch_list: ', batch_list)
+    if torch.is_complex(windows):
+        res = torch.complex(torch.zeros([B, C, H, W]), torch.zeros([B, C, H, W]))
+        res = res.to(windows.device)
+    else:
+        res = torch.zeros([B, C, H, W], device=windows.device)
+
+    res[:, :, :h, :w] = x_main
+    if h == H and w == W:
+        return res
+    if h != H and w != W and len(batch_list) == 4:
+        x_dd = window_reverses(windows[batch_list[2]:, ...], window_size, window_size, window_size)
+        res[:, :, h:, w:] = x_dd[:, :, h - H:, w - W:]
+        x_r = window_reverses(windows[batch_list[0]:batch_list[1], ...], window_size, h, window_size)
+        res[:, :, :h, w:] = x_r[:, :, :, w - W:]
+        x_d = window_reverses(windows[batch_list[1]:batch_list[2], ...], window_size, window_size, w)
+        res[:, :, h:, :w] = x_d[:, :, h - H:, :]
+        return res
+    if w != W and len(batch_list) == 2:
+        x_r = window_reverses(windows[batch_list[0]:batch_list[1], ...], window_size, h, window_size)
+        res[:, :, :h, w:] = x_r[:, :, :, w - W:]
+    if h != H and len(batch_list) == 2:
+        x_d = window_reverses(windows[batch_list[0]:batch_list[1], ...], window_size, window_size, w)
+        res[:, :, h:, :w] = x_d[:, :, h - H:, :]
+    return res
+class frequency_feature(nn.Module): 
+    def __init__(self, dim, dw=1, norm='backward', act_method=nn.GELU, window_size=None, bias=False):
+        super(frequency_feature, self).__init__()
+        self.act_fft = act_method()
+        self.window_size = window_size
+        hid_dim = dim * dw
+
+        self.complex_weight1_real = nn.Parameter(torch.Tensor(dim, hid_dim))
+        self.complex_weight1_imag = nn.Parameter(torch.Tensor(dim, hid_dim))
+        self.complex_weight2_real = nn.Parameter(torch.Tensor(hid_dim, dim))
+        self.complex_weight2_imag = nn.Parameter(torch.Tensor(hid_dim, dim))
+        init.kaiming_uniform_(self.complex_weight1_real, a=math.sqrt(16))
+        init.kaiming_uniform_(self.complex_weight1_imag, a=math.sqrt(16))
+        init.kaiming_uniform_(self.complex_weight2_real, a=math.sqrt(16))
+        init.kaiming_uniform_(self.complex_weight2_imag, a=math.sqrt(16))
+        if bias:
+            self.b1_real = nn.Parameter(torch.zeros((1, 1, 1, hid_dim)), requires_grad=True)
+            self.b1_imag = nn.Parameter(torch.zeros((1, 1, 1, hid_dim)), requires_grad=True)
+            self.b2_real = nn.Parameter(torch.zeros((1, 1, 1, dim)), requires_grad=True)
+            self.b2_imag = nn.Parameter(torch.zeros((1, 1, 1, dim)), requires_grad=True)
+        self.bias = bias
+        self.norm = norm
+        # self.BatchNorm = nn.BatchNorm2d(hid_dim*2)
+        # self.min = inf
+        # self.max = -inf
+
+    def forward(self, x):
+        _, _, H, W = x.shape
+        if self.window_size is not None and (H != self.window_size or W != self.window_size):
+            x, batch_list = window_partitionx(x, self.window_size)
+        y = torch.fft.rfft2(x, norm=self.norm)
+        dim = 1
+        weight1 = torch.complex(self.complex_weight1_real, self.complex_weight1_imag)
+        weight2 = torch.complex(self.complex_weight2_real, self.complex_weight2_imag)
+        if self.bias:
+            b1 = torch.complex(self.b1_real, self.b1_imag)
+            b2 = torch.complex(self.b2_real, self.b2_imag)
+        y = rearrange(y, 'b c h w -> b h w c')
+        y = y @ weight1
+        if self.bias:
+            y = y + b1
+        y = torch.cat([y.real, y.imag], dim=dim)
+        # y = self.BatchNorm(y)
+        y = self.act_fft(y)
+        y_real, y_imag = torch.chunk(y, 2, dim=dim)
+        y = torch.complex(y_real, y_imag)
+        y = y @ weight2
+        if self.bias:
+            y = y + b2
+        y = rearrange(y, 'b h w c -> b c h w')
+
+        if self.window_size is not None and (H != self.window_size or W != self.window_size):
+            y = torch.fft.irfft2(y, s=(self.window_size, self.window_size), norm=self.norm)
+            y = window_reversex(y, self.window_size, H, W, batch_list)
+        else:
+            y = torch.fft.irfft2(y, s=(H, W), norm=self.norm)
+        return y
---
+++ basicsr/Spatial_branch.py
@@ -0,0 +1,383 @@
+import torch
+import itertools
+import torch.nn as nn
+from timm.models.vision_transformer import trunc_normal_
+from timm.layers import SqueezeExcite
+import torch.nn.functional as F
+from functools import partial
+from timm.layers import DropPath
+import math
+import torch.nn.functional as F
+from functools import partial
+from typing import Optional, Callable
+from basicsr.utils.registry import ARCH_REGISTRY
+from timm.layers import DropPath, to_2tuple, trunc_normal_
+from mamba_ssm.ops.selective_scan_interface import selective_scan_fn, selective_scan_ref
+from einops import rearrange, repeat
+from torch.nn import init as init
+
+from basicsr.utils.utils import *
+class _ScaleModule(nn.Module):
+    def __init__(self, dims, init_scale=1.0, init_bias=0):
+        super(_ScaleModule, self).__init__()
+        self.dims = dims
+        self.weight = nn.Parameter(torch.ones(*dims) * init_scale)
+        self.bias = None
+
+    def forward(self, x):
+        return torch.mul(self.weight, x)
+
+class DWConv2d_BN_ReLU(nn.Sequential):
+    def __init__(self, in_channels, out_channels, kernel_size=3, bn_weight_init=1):
+        super().__init__()
+        self.add_module('dwconv3x3',
+                        nn.Conv2d(in_channels, in_channels, kernel_size=kernel_size, stride=1, padding=kernel_size//2, groups=in_channels,
+                                  bias=False))
+        self.add_module('bn1', nn.BatchNorm2d(in_channels))
+        self.add_module('relu', nn.ReLU(inplace=True))
+        self.add_module('dwconv1x1',
+                        nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0, groups=in_channels,
+                                  bias=False))
+        self.add_module('bn2', nn.BatchNorm2d(out_channels))
+
+        # Initialize batch norm weights
+        nn.init.constant_(self.bn1.weight, bn_weight_init)
+        nn.init.constant_(self.bn1.bias, 0)
+        nn.init.constant_(self.bn2.weight, bn_weight_init)
+        nn.init.constant_(self.bn2.bias, 0)
+
+    @torch.no_grad()
+    def fuse(self):
+        dwconv3x3, bn1, relu, dwconv1x1, bn2 = self._modules.values()
+
+        w1 = bn1.weight / (bn1.running_var + bn1.eps) ** 0.5
+        w1 = dwconv3x3.weight * w1[:, None, None, None]
+        b1 = bn1.bias - bn1.running_mean * bn1.weight / (bn1.running_var + bn1.eps) ** 0.5
+
+        fused_dwconv3x3 = nn.Conv2d(w1.size(1) * dwconv3x3.groups, w1.size(0), w1.shape[2:], stride=dwconv3x3.stride,
+                                    padding=dwconv3x3.padding, dilation=dwconv3x3.dilation, groups=dwconv3x3.groups,
+                                    device=dwconv3x3.weight.device)
+        fused_dwconv3x3.weight.data.copy_(w1)
+        fused_dwconv3x3.bias.data.copy_(b1)
+
+        # Fuse dwconv1x1 and bn2
+        w2 = bn2.weight / (bn2.running_var + bn2.eps) ** 0.5
+        w2 = dwconv1x1.weight * w2[:, None, None, None]
+        b2 = bn2.bias - bn2.running_mean * bn2.weight / (bn2.running_var + bn2.eps) ** 0.5
+
+        fused_dwconv1x1 = nn.Conv2d(w2.size(1) * dwconv1x1.groups, w2.size(0), w2.shape[2:], stride=dwconv1x1.stride,
+                                    padding=dwconv1x1.padding, dilation=dwconv1x1.dilation, groups=dwconv1x1.groups,
+                                    device=dwconv1x1.weight.device)
+        fused_dwconv1x1.weight.data.copy_(w2)
+        fused_dwconv1x1.bias.data.copy_(b2)
+
+        # Create a new sequential model with fused layers
+        fused_model = nn.Sequential(fused_dwconv3x3, relu, fused_dwconv1x1)
+        return fused_model
+
+class Conv2d_BN(torch.nn.Sequential):
+    def __init__(self, a, b, ks=1, stride=1, pad=0, dilation=1,
+                 groups=1, bn_weight_init=1,):
+        super().__init__()
+        self.add_module('c', torch.nn.Conv2d(
+            a, b, ks, stride, pad, dilation, groups, bias=False))
+        self.add_module('bn', torch.nn.BatchNorm2d(b))
+        torch.nn.init.constant_(self.bn.weight, bn_weight_init)
+        torch.nn.init.constant_(self.bn.bias, 0)
+
+    @torch.no_grad()
+    def fuse(self):
+        c, bn = self._modules.values()
+        w = bn.weight / (bn.running_var + bn.eps) ** 0.5
+        w = c.weight * w[:, None, None, None]
+        b = bn.bias - bn.running_mean * bn.weight / \
+            (bn.running_var + bn.eps) ** 0.5
+        m = torch.nn.Conv2d(w.size(1) * self.c.groups, w.size(
+            0), w.shape[2:], stride=self.c.stride, padding=self.c.padding, dilation=self.c.dilation,
+                            groups=self.c.groups)
+        m.weight.data.copy_(w)
+        m.bias.data.copy_(b)
+        return m
+
+
+class BN_Linear(torch.nn.Sequential):
+    def __init__(self, a, b, bias=True, std=0.02):
+        super().__init__()
+        self.add_module('bn', torch.nn.BatchNorm1d(a))
+        self.add_module('l', torch.nn.Linear(a, b, bias=bias))
+        trunc_normal_(self.l.weight, std=std)
+        if bias:
+            torch.nn.init.constant_(self.l.bias, 0)
+
+    @torch.no_grad()
+    def fuse(self):
+        bn, l = self._modules.values()
+        w = bn.weight / (bn.running_var + bn.eps) ** 0.5
+        b = bn.bias - self.bn.running_mean * \
+            self.bn.weight / (bn.running_var + bn.eps) ** 0.5
+        w = l.weight * w[None, :]
+        if l.bias is None:
+            b = b @ self.l.weight.T
+        else:
+            b = (l.weight @ b[:, None]).view(-1) + self.l.bias
+        m = torch.nn.Linear(w.size(1), w.size(0))
+        m.weight.data.copy_(w)
+        m.bias.data.copy_(b)
+        return m
+
+
+class PatchMerging(torch.nn.Module):
+    def __init__(self, dim, out_dim):
+        super().__init__()
+        hid_dim = int(dim * 4)
+        self.conv1 = Conv2d_BN(dim, hid_dim, 1, 1, 0, )
+        self.act = torch.nn.ReLU()
+        self.conv2 = Conv2d_BN(hid_dim, hid_dim, 3, 2, 1, groups=hid_dim,)
+        self.se = SqueezeExcite(hid_dim, .25)
+        self.conv3 = Conv2d_BN(hid_dim, out_dim, 1, 1, 0,)
+
+    def forward(self, x):
+        x = self.conv3(self.se(self.act(self.conv2(self.act(self.conv1(x))))))
+        return x
+
+
+class Residual(torch.nn.Module):
+    def __init__(self, m, drop=0.):
+        super().__init__()
+        self.m = m
+        self.drop = drop
+
+    def forward(self, x):
+        if self.training and self.drop > 0:
+            return x + self.m(x) * torch.rand(x.size(0), 1, 1, 1,
+                                              device=x.device).ge_(self.drop).div(1 - self.drop).detach()
+        else:
+            return x + self.m(x)
+
+
+def nearest_multiple_of_16(n):
+    if n % 16 == 0:
+        return n
+    else:
+        lower_multiple = (n // 16) * 16
+        upper_multiple = lower_multiple + 16
+
+        if (n - lower_multiple) < (upper_multiple - n):
+            return lower_multiple
+        else:
+            return upper_multiple
+
+class SS2D(nn.Module):
+    def __init__(
+            self,
+            d_model,  
+            d_state=16, 
+            d_conv=3, 
+            expand=2.,
+            dt_rank="auto",
+            dt_min=0.001,
+            dt_max=0.1,
+            dt_init="random",
+            dt_scale=1.0, 
+            dt_init_floor=1e-4,
+            dropout=0., 
+            conv_bias=True, 
+            bias=False,
+            device=None,
+            dtype=None,
+            **kwargs,
+    ):
+        factory_kwargs = {"device": device, "dtype": dtype}
+        super().__init__()
+        self.d_model = d_model
+        self.d_state = d_state
+        self.d_conv = d_conv
+        self.expand = expand
+        self.d_inner = int(self.expand * self.d_model)
+        self.dt_rank = math.ceil(self.d_model / 16) if dt_rank == "auto" else dt_rank
+
+        self.in_proj = nn.Linear(self.d_model, self.d_inner * 2, bias=bias, **factory_kwargs)
+        self.conv2d = nn.Conv2d(
+            in_channels=self.d_inner,
+            out_channels=self.d_inner,
+            groups=self.d_inner,
+            bias=conv_bias,
+            kernel_size=d_conv,
+            padding=(d_conv - 1) // 2,
+            **factory_kwargs,
+        )
+        self.act = nn.SiLU()
+
+        self.x_proj = (
+            #d_inner=
+            nn.Linear(self.d_inner, (self.dt_rank + self.d_state * 2), bias=False, **factory_kwargs),
+            nn.Linear(self.d_inner, (self.dt_rank + self.d_state * 2), bias=False, **factory_kwargs),
+            nn.Linear(self.d_inner, (self.dt_rank + self.d_state * 2), bias=False, **factory_kwargs),
+            nn.Linear(self.d_inner, (self.dt_rank + self.d_state * 2), bias=False, **factory_kwargs),
+        )
+        self.x_proj_weight = nn.Parameter(torch.stack([t.weight for t in self.x_proj], dim=0))  # (K=4, N, inner)
+        del self.x_proj
+
+        self.dt_projs = (
+            self.dt_init(self.dt_rank, self.d_inner, dt_scale, dt_init, dt_min, dt_max, dt_init_floor,
+                         **factory_kwargs),
+            self.dt_init(self.dt_rank, self.d_inner, dt_scale, dt_init, dt_min, dt_max, dt_init_floor,
+                         **factory_kwargs),
+            self.dt_init(self.dt_rank, self.d_inner, dt_scale, dt_init, dt_min, dt_max, dt_init_floor,
+                         **factory_kwargs),
+            self.dt_init(self.dt_rank, self.d_inner, dt_scale, dt_init, dt_min, dt_max, dt_init_floor,
+                         **factory_kwargs),
+        )
+        self.dt_projs_weight = nn.Parameter(torch.stack([t.weight for t in self.dt_projs], dim=0))  # (K=4, inner, rank)
+        self.dt_projs_bias = nn.Parameter(torch.stack([t.bias for t in self.dt_projs], dim=0))  # (K=4, inner)
+        del self.dt_projs
+
+        self.A_logs = self.A_log_init(self.d_state, self.d_inner, copies=4, merge=True)  # (K=4, D, N)
+        self.Ds = self.D_init(self.d_inner, copies=4, merge=True)  # (K=4, D, N)
+
+        self.selective_scan = selective_scan_fn
+
+        self.out_norm = nn.LayerNorm(self.d_inner)
+        self.out_proj = nn.Linear(self.d_inner, self.d_model, bias=bias, **factory_kwargs)
+        self.dropout = nn.Dropout(dropout) if dropout > 0. else None
+
+    @staticmethod
+    def dt_init(dt_rank, d_inner, dt_scale=1.0, dt_init="random", dt_min=0.001, dt_max=0.1, dt_init_floor=1e-4,
+                **factory_kwargs):
+        dt_proj = nn.Linear(dt_rank, d_inner, bias=True, **factory_kwargs)
+
+        # Initialize special dt projection to preserve variance at initialization
+        dt_init_std = dt_rank ** -0.5 * dt_scale
+        if dt_init == "constant":
+            nn.init.constant_(dt_proj.weight, dt_init_std)
+        elif dt_init == "random":
+            nn.init.uniform_(dt_proj.weight, -dt_init_std, dt_init_std)
+        else:
+            raise NotImplementedError
+
+        # Initialize dt bias so that F.softplus(dt_bias) is between dt_min and dt_max
+        dt = torch.exp(
+            torch.rand(d_inner, **factory_kwargs) * (math.log(dt_max) - math.log(dt_min))
+            + math.log(dt_min)
+        ).clamp(min=dt_init_floor)
+        inv_dt = dt + torch.log(-torch.expm1(-dt))
+        with torch.no_grad():
+            dt_proj.bias.copy_(inv_dt)
+        # Our initialization would set all Linear.bias to zero, need to mark this one as _no_reinit
+        dt_proj.bias._no_reinit = True
+
+        return dt_proj
+
+    @staticmethod
+    def A_log_init(d_state, d_inner, copies=1, device=None, merge=True):
+        # S4D real initialization
+        A = repeat(
+            torch.arange(1, d_state + 1, dtype=torch.float32, device=device),
+            "n -> d n",
+            d=d_inner,
+        ).contiguous()
+        A_log = torch.log(A)  # Keep A_log in fp32
+        if copies > 1:
+            A_log = repeat(A_log, "d n -> r d n", r=copies)
+            if merge:
+                A_log = A_log.flatten(0, 1)
+        A_log = nn.Parameter(A_log)
+        A_log._no_weight_decay = True
+        return A_log
+
+    @staticmethod
+    def D_init(d_inner, copies=1, device=None, merge=True):
+        # D "skip" parameter
+        D = torch.ones(d_inner, device=device)
+        if copies > 1:
+            D = repeat(D, "n1 -> r n1", r=copies)
+            if merge:
+                D = D.flatten(0, 1)
+        D = nn.Parameter(D)  # Keep in fp32
+        D._no_weight_decay = True
+        return D
+
+    def forward_core(self, x: torch.Tensor):
+        B, C, H, W = x.shape
+        L = H * W
+        K = 4
+        x_hwwh = torch.stack([x.view(B, -1, L), torch.transpose(x, dim0=2, dim1=3).contiguous().view(B, -1, L)], dim=1).view(B, 2, -1, L)
+        xs = torch.cat([x_hwwh, torch.flip(x_hwwh, dims=[-1])], dim=1) # (1, 4, 192, 3136)
+
+        x_dbl = torch.einsum("b k d l, k c d -> b k c l", xs.view(B, K, -1, L), self.x_proj_weight)
+        dts, Bs, Cs = torch.split(x_dbl, [self.dt_rank, self.d_state, self.d_state], dim=2)
+        dts = torch.einsum("b k r l, k d r -> b k d l", dts.view(B, K, -1, L), self.dt_projs_weight)
+        xs = xs.float().view(B, -1, L)
+        dts = dts.contiguous().float().view(B, -1, L) # (b, k * d, l)
+        print("+++++++++++++")
+        print(dts.shape)
+        Bs = Bs.float().view(B, K, -1, L)
+        Cs = Cs.float().view(B, K, -1, L) # (b, k, d_state, l)
+        Ds = self.Ds.float().view(-1)
+        As = -torch.exp(self.A_logs.float()).view(-1, self.d_state)
+        print("----------------")
+        print(As.shape)
+        dt_projs_bias = self.dt_projs_bias.float().view(-1) # (k * d)
+        out_y = self.selective_scan(
+            xs, dts,
+            As, Bs, Cs, Ds, z=None,
+            delta_bias=dt_projs_bias,
+            delta_softplus=True,
+            return_last_state=False,
+        ).view(B, K, -1, L)
+        assert out_y.dtype == torch.float
+
+        inv_y = torch.flip(out_y[:, 2:4], dims=[-1]).view(B, 2, -1, L)
+        wh_y = torch.transpose(out_y[:, 1].view(B, -1, W, H), dim0=2, dim1=3).contiguous().view(B, -1, L)
+        invwh_y = torch.transpose(inv_y[:, 1].view(B, -1, W, H), dim0=2, dim1=3).contiguous().view(B, -1, L)
+
+        return out_y[:, 0], inv_y[:, 0], wh_y, invwh_y
+
+    def forward(self, x: torch.Tensor, **kwargs):
+        B, H, W, C = x.shape #
+        xz = self.in_proj(x) #
+        x, z = xz.chunk(2, dim=-1)
+        x = x.permute(0, 3, 1, 2).contiguous()
+        x = self.act(self.conv2d(x))
+        y1, y2, y3, y4 = self.forward_core(x)
+        assert y1.dtype == torch.float32
+        y = y1 + y2 + y3 + y4
+        y = torch.transpose(y, dim0=1, dim1=2).contiguous().view(B, H, W, -1)
+        y = self.out_norm(y)
+        y = y * F.silu(z)
+        out = self.out_proj(y)
+        if self.dropout is not None:
+            out = self.dropout(out)
+        return out
+
+class Spatial_branch(torch.nn.Module):
+    def __init__(self, dim, d_state,expand,dropout, part1=0.25, part2=0.25,
+                 kernels=3,):
+        super().__init__()
+        self.dim = dim 
+        self.global_channels = nearest_multiple_of_16(int(part1 * dim)) 
+        if self.global_channels + int(part2 * dim) > dim:
+            self.local_channels = dim - self.global_channels
+        else:
+            self.local_channels = int(part2 * dim)
+        self.identity_channels = self.dim - self.global_channels - self.local_channels
+
+        if self.local_channels != 0:
+            self.local_op = DWConv2d_BN_ReLU(self.local_channels, self.local_channels,kernels) 
+        else:
+            self.local_op = nn.Identity() 
+        if self.global_channels != 0:
+            self.global_op = SS2D(d_model=self.global_channels, d_state=d_state,expand=expand,dropout=dropout, kernels=kernels,) 
+        else:
+            self.global_op = nn.Identity()
+
+        self.proj = torch.nn.Sequential(torch.nn.ReLU(), Conv2d_BN(
+            dim, dim, bn_weight_init=0,))
+
+    def forward(self, x):  
+        x1, x2, x3 = torch.split(x, [self.global_channels, self.local_channels, self.identity_channels], dim=1)
+        x1 = self.global_op(x1)
+        x2 = self.local_op(x2)
+        x = self.proj(torch.cat([x1, x2, x3], dim=1))
+        return x
+
